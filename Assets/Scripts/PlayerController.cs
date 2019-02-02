c7bd4b287f4d2c8e8c5bf1541cc6f024a0f582fd
Wallace <wallace.balaniuc@gmail.com>
2019-02-01 20:41:25 -0500

    Added player with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        // Debug.Log("H: " + horiz + " , V: " + vert);
+
+        rBody.velocity = new Vector2(horiz * speed, vert * speed
+            );
+    }
+}
