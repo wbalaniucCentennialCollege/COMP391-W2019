5f4c721d86de0db23b5a144c44f44f68e263ff56
Wallace <wallace.balaniuc@gmail.com>
2019-02-01 21:25:25 -0500

    Bound the character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Public Variables
     public float speed = 10.0f;
+
+    // Private Variables
+    private Rigidbody2D rBody;
      
     // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,14 +24,17 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
-
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
 
         // Debug.Log("H: " + horiz + " , V: " + vert);
+        Vector2 newVelocity = new Vector2(horiz, vert);
 
-        rBody.velocity = new Vector2(horiz * speed, vert * speed
-            );
+        rBody.velocity = newVelocity * speed;
+
+        // Restrict the player from leaving the play area
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, -8, 4),   // Restrict x position to -8 and 4
+            Mathf.Clamp(rBody.position.y, -4, 4));  // Restrict y position to -4 and 4
     }
 }
