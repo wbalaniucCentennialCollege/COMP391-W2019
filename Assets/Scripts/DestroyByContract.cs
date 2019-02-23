7cb809bec2ad7a8a6a360834f07fe544bd9e0c4e
Wallace <wallace.balaniuc@gmail.com>
2019-02-22 21:26:03 -0500

    Added asteroids and tumble and destruction

---
+++ Assets/Scripts/DestroyByContract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContract : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+
+        // Debug.Log(other.gameObject.name);
+        
+        Destroy(other.gameObject);  // Laser
+        Destroy(this.gameObject);   // Asteroid
+    }
+}
