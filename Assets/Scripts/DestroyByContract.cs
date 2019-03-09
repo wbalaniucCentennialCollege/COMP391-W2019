51a77380509b5b05e06709b4802f7bda72ccac4a
Wallace <wallace.balaniuc@gmail.com>
2019-03-08 21:26:27 -0500

    Added player explosion

--- Assets/Scripts/DestroyByContract.cs
+++ Assets/Scripts/DestroyByContract.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyByContract : MonoBehaviour
 {
     public GameObject explosion;
+    public GameObject explosionPlayer;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,13 +17,15 @@ public class DestroyByContract : MonoBehaviour
         if(other.tag == "Player")
         {
             // Trigger game over logic in here
+            GameObject temp = Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+            Destroy(temp, 2.0f);
             Destroy(other.gameObject);
         }
 
         // Debug.Log(other.gameObject.name);
 
         // Instantiate asteroid explosion animation
-        Instantiate(explosion, this.transform.position, Quaternion.identity);
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
 
         Destroy(other.gameObject);  // Laser
         Destroy(this.gameObject);   // Asteroid
