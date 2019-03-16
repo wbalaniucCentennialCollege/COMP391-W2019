438f70ebb6c09f1f005ec76960e039d4f23370ef
Wallace <wallace.balaniuc@gmail.com>
2019-03-15 21:33:41 -0400

    Asteroids spawned with waves

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Define some variables that control spawning my waves of enemies
+    public GameObject hazard;   // What are we spawning?
+    public Vector2 spawnValue;  // Where do we spawn our hazards?
+    public int hazardCount;     // How many hazards per wave?
+    public float startWait;     // How long until the first wave?
+    public float spawnWait;     // How much time between each hazard in a wave?
+    public float waveWait;      // How long between each wave of hazard?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Run a separate function from the rest of the code
+        // In it's own thread
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // IEnumerator return type is required for Coroutines
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); // "Pause". This will "wait" for "startWait" seconds
+        while(true) // Now we want to spawn some stuff...
+        {
+            
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawnValue.x, Random.Range(-spawnValue.y, spawnValue.y));
+
+                Instantiate(hazard, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait); // Wait time between spawning each asteroid
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+    
+}
